629714bdcf8c3ecc658fa50ccb71685bb582307d
Shweta <96809628+shwetadhote@users.noreply.github.com>
2022-02-03 11:55:47 +0530

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,10 @@
+const obj = {}
+const keyArray = ['firstName','lastName','address', 'state'];
+const valueArray = ['Abcde' , 'Xyz' , 'Indore', 'Madhya Pradesh'];
+
+obj[keyArray[0]] = valueArray[0]
+obj[keyArray[1]] = valueArray[1]
+obj[keyArray[2]] = valueArray[2]
+obj[keyArray[3]] = valueArray[3]
+
+console.log(obj);
